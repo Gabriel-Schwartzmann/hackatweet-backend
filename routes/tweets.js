69d943b93553f184f0fdc764bb4acc0b9e21ef82
Gabriel <gabriel.schwartzmann@free.fr>
2024-10-10 11:37:59 +0200

    added all routes

--- routes/tweets.js
+++ routes/tweets.js
@@ -31,18 +31,131 @@ router.post('/', (req, res) => {
 })
 
 router.get('all/:token', (req, res) => {
-    User.findOne({ token : req.params.token }).then(userData => {
-        if(userData === null) {
-            res.json({ result: false, error:'User not found'})
+    User.findOne({ token: req.params.token }).then(userData => {
+        if (userData === null) {
+            res.json({ result: false, error: 'User not found' })
             return;
         }
-        Tweet.find() // Populate and select specific fields to return (for security purposes)
-      .populate('author', ['username', 'firstName'])
-      .populate('likes', ['username'])
-      .sort({ createdOn: 'desc' })
-      .then(tweets => {
-        res.json({ result: true, tweets });
-      });
-  });
+        Tweet.find()
+            .populate('author', ['username', 'firstName'])
+            .populate('likes', ['username'])
+            .sort({ createdOn: 'desc' })
+            .then(tweets => {
+                res.json({ result: true, tweets });
+            });
+    });
 });
-   
+
+router.get('/trends/:token', (req, res) => {
+    User.findOne({ token: req.params.token }).then(user => {
+        if (user === null) {
+            res.json({ result: false, error: 'User not found' });
+            return;
+        }
+
+        Tweet.find({ content: { $regex: /#/ } })
+            .then(tweets => {
+                const hashtags = [];
+
+                for (const tweet of tweets) {
+                    const filteredHashtags = tweet.content.split(' ').filter(word => word.startsWith('#') && word.length > 1);
+                    hashtags.push(...filteredHashtags);
+                }
+
+                const trends = [];
+                for (const hashtag of hashtags) {
+                    const trendIndex = trends.findIndex(trend => trend.hashtag === hashtag);
+                    if (trendIndex === -1) {
+                        trends.push({ hashtag, count: 1 });
+                    } else {
+                        trends[trendIndex].count++;
+                    }
+                }
+
+                res.json({ result: true, trends: trends.sort((a, b) => b.count - a.count) });
+            });
+    });
+});
+
+router.get('/hashtag/:token/:query', (req, res) => {
+    User.findOne({ token: req.params.token }).then(user => {
+        if (user === null) {
+            res.json({ result: false, error: 'User not found' });
+            return;
+        }
+
+        Tweet.find({ content: { $regex: new RegExp('#' + req.params.query, 'i') } })
+            .populate('author', ['username', 'firstName'])
+            .populate('likes', ['username'])
+            .sort({ createdAt: 'desc' })
+            .then(tweets => {
+                res.json({ result: true, tweets });
+            });
+    });
+});
+
+router.delete('/', (req, res) => {
+    if (!checkBody(req.body, ['token', 'tweetId'])) {
+        res.json({ result: false, error: 'Missing or empty fields' });
+        return;
+    }
+
+    User.findOne({ token: req.body.token }).then(user => {
+        if (user === null) {
+            res.json({ result: false, error: 'User not found' });
+            return;
+        }
+
+        Tweet.findById(req.body.tweetId)
+            .populate('author')
+            .then(tweet => {
+                if (!tweet) {
+                    res.json({ result: false, error: 'Tweet not found' });
+                    return;
+                } else if (String(tweet.author._id) !== String(user._id)) { 
+                    res.json({ result: false, error: 'Tweet can only be deleted by its author' });
+                    return;
+                }
+
+                Tweet.deleteOne({ _id: tweet._id }).then(() => {
+                    res.json({ result: true });
+                });
+            });
+    });
+});
+
+router.put('/like', (req, res) => {
+    if (!checkBody(req.body, ['token', 'tweetId'])) {
+        res.json({ result: false, error: 'Missing or empty fields' });
+        return;
+    }
+
+    User.findOne({ token: req.body.token }).then(user => {
+        if (user === null) {
+            res.json({ result: false, error: 'User not found' });
+            return;
+        }
+
+        Tweet.findById(req.body.tweetId).then(tweet => {
+            if (!tweet) {
+                res.json({ result: false, error: 'Tweet not found' });
+                return;
+            }
+
+            if (tweet.likes.includes(user._id)) {
+                Tweet.updateOne({ _id: tweet._id }, { $pull: { likes: user._id } })
+                    .then(() => {
+                        res.json({ result: true });
+                    });
+            } else {
+                Tweet.updateOne({ _id: tweet._id }, { $push: { likes: user._id } })
+                    .then(() => {
+                        res.json({ result: true });
+                    });
+            }
+        });
+    });
+});
+
+
+module.exports = router;
